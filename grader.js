b79b5aeae720ccedd0966529b0a24b4fd02109e7
Marco Tessari <marco.tessari@gmail.com>
2013-07-09 16:47:50 +0000

    Upgrade for HW3

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -36,16 +37,16 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+var cheerioHtmlContent = function(htmlContent) {
+    return cheerio.load(htmlContent);
 };
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtmlContent = function(htmlContent, checksfile) {
+    $ = cheerioHtmlContent(htmlContent);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -61,14 +62,42 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var checkAndDisplay = function(htmlContent, checkFile) {
+    var checkJson = checkHtmlContent(htmlContent, checkFile);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+};
+
+var check = function(filename, url, checkFile) {
+    if (undefined !== url) {
+        rest.get(url).on('complete', function(result, response) {
+            if (result instanceof Error) {
+                console.error('Error: ' + util.format(response.message));
+                process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+            } else {
+                checkAndDisplay(result, checkFile);
+            }
+        });
+    } else {
+        fs.readFile(filename, function(err, data) {
+            if (err) {
+                console.error('Error: ' + err);
+                process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+            } else {
+                checkAndDisplay(data, checkFile);
+            }
+        });
+    }
+};
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-f, --file [html_file]', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url [url_to_html_file]', 'URL to index.html')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    check(program.file, program.url, program.checks);
 } else {
-    exports.checkHtmlFile = checkHtmlFile;
+    exports.checkHtmlContent = checkHtmlContent;
 }
